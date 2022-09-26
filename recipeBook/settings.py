1ff38d85d35b587cb92848ed6a9407d912ce1774
F4ruk-seker <ahmetfaruk731@gmail.com>
2022-09-26 12:09:22 +0300

    secure setting level 2 basic configration

--- recipeBook/settings.py
+++ recipeBook/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=2uc%&@p4x_x6v9q(5ja(jlkr(32qhg9^)=(9yz+^=#pu@*!r@'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 
 # Application definition
