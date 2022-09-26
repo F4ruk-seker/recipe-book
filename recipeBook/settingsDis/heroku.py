be3d19a139d3cb8312a921f306128c7b367fff5f
F4ruk-seker <ahmetfaruk731@gmail.com>
2022-09-26 03:28:14 +0300

    heroku proc file

--- recipeBook/settingsDis/heroku.py
+++ recipeBook/settingsDis/heroku.py
@@ -2,9 +2,3 @@ import django_heroku
 
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
-
-django_heroku.settings(locals())
